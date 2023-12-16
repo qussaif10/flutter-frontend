6b25c0dea99c7c92552f965e7217cf220e27432c
qussaif10 <qussaif100+1@gmail.com>
2023-12-16 21:12:14 +0300

    made a moving button clicker game

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -9,17 +10,78 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.white,
-          background: Colors.white
+      home: Scaffold(body: MovingButton()),
+    );
+  }
+}
+
+class MovingButton extends StatefulWidget {
+  const MovingButton({super.key});
+
+  @override
+  State<MovingButton> createState() => _MovingButtonState();
+}
+
+class _MovingButtonState extends State<MovingButton> {
+  Random random = Random();
+  int count = 0;
+
+  double x = 0;
+  double y = 0;
+  int yMax = 690;
+  int xMax = 336;
+
+  void changePosition() {
+    setState(() {
+      x = random.nextInt(xMax + 1).toDouble();
+      y = random.nextInt(yMax + 1).toDouble();
+      count++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text(
+            "Moving Button",
+            style: TextStyle(color: Colors.white),
+          ),
         ),
-        useMaterial3: true
+        backgroundColor: Colors.indigo,
       ),
-      home: Scaffold()
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            left: x,
+            top: y,
+            child: IconButton(
+              onPressed: () {
+                changePosition();
+              },
+              icon: const Icon(Icons.add_box),
+              iconSize: 60.0,
+              highlightColor: Colors.transparent
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              margin: const EdgeInsets.all(20),
+              child: Text(
+                "Count: $count",
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
