2ad42f6cf96fba3c65c840ce5c4c2bff9a75bca4
qussaif10 <qussaif100+1@gmail.com>
2023-12-17 20:11:03 +0300

    making login form

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,70 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: LoginForm(),
     );
+  }
+}
+
+class LoginForm extends StatefulWidget {
+  const LoginForm({super.key});
+
+  @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(2),
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.black,
+              width: 1,
+            ),
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Form(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Text(
+                    "Login",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: "Username",
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SingUpForm extends StatefulWidget {
+  const SingUpForm({super.key});
+
+  @override
+  State<SingUpForm> createState() => _SingUpFormState();
+}
+
+class _SingUpFormState extends State<SingUpForm> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
