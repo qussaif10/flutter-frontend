e83a18cd6615d7ca9fa4d82b01b364de3ebb125c
qussaif10 <qussaif100+1@gmail.com>
2023-12-17 15:55:27 +0300

    branch cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -12,76 +11,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(body: MovingButton()),
-    );
-  }
-}
-
-class MovingButton extends StatefulWidget {
-  const MovingButton({super.key});
-
-  @override
-  State<MovingButton> createState() => _MovingButtonState();
-}
-
-class _MovingButtonState extends State<MovingButton> {
-  Random random = Random();
-  int count = 0;
-
-  double x = 0;
-  double y = 0;
-  int yMax = 690;
-  int xMax = 336;
-
-  void changePosition() {
-    setState(() {
-      x = random.nextInt(xMax + 1).toDouble();
-      y = random.nextInt(yMax + 1).toDouble();
-      count++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Center(
-          child: Text(
-            "Moving Button",
-            style: TextStyle(color: Colors.white),
-          ),
-        ),
-        backgroundColor: Colors.indigo,
-      ),
-      body: Stack(
-        children: <Widget>[
-          Positioned(
-            left: x,
-            top: y,
-            child: IconButton(
-              onPressed: () {
-                changePosition();
-              },
-              icon: const Icon(Icons.add_box),
-              iconSize: 60.0,
-              highlightColor: Colors.transparent
-            ),
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              margin: const EdgeInsets.all(20),
-              child: Text(
-                "Count: $count",
-                style: const TextStyle(
-                  color: Colors.black,
-                  fontSize: 24,
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
