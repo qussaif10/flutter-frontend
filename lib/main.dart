034a2941515dcca56ae5a2b3e33eb0917be4e6d2
qussaif10 <qussaif100+1@gmail.com>
2023-12-28 17:28:33 +0300

    Implement login functionality and response handling
    Added the http package to the project for handling network requests. The login form now attempts to make a post request upon login button press. After the request, it navigates to either a success or failure screen depending on the response. Dispose methods have been implemented to properly clean up text editing controllers from memory when they are no longer needed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -24,6 +26,17 @@ class LoginForm extends StatefulWidget {
 }
 
 class _LoginFormState extends State<LoginForm> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,6 +54,7 @@ class _LoginFormState extends State<LoginForm> {
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: Form(
+              key: _formKey,
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
@@ -54,6 +68,7 @@ class _LoginFormState extends State<LoginForm> {
                   Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: TextFormField(
+                      controller: _usernameController,
                       decoration: InputDecoration(
                         hintText: "Username",
                         border: OutlineInputBorder(
@@ -65,21 +80,69 @@ class _LoginFormState extends State<LoginForm> {
                   Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: TextFormField(
+                      controller: _passwordController,
                       decoration: InputDecoration(
                         hintText: "Password",
                         border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(5),
                         ),
                       ),
+                      obscureText: true,
                     ),
                   ),
                   ElevatedButton(
-                    onPressed: () {
-                      // Handle login logic
+                    onPressed: () async {
+                      try {
+                        if (_formKey.currentState!.validate()) {
+                          var url = Uri.parse("http://10.0.0.2:8221/login");
+                          var response = await http.post(url, body: {
+                            'username': _usernameController.text,
+                            'password': _passwordController.text,
+                          });
+
+                          if (mounted) {
+                            if (response.statusCode == 200) {
+                              if (kDebugMode) {
+                                print("Login successful: ${response.body}");
+                              }
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const SuccessScreen(),
+                                ),
+                              );
+                            } else {
+                              if (kDebugMode) {
+                                print("Login Failed: ${response.statusCode}");
+                              }
+
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const FailScreen(),
+                                ),
+                              );
+                            }
+                          }
+                        }
+                      } catch (e) {
+                        if (kDebugMode) {
+                          print(e);
+                        }
+                        // Handle the error, for example by showing an alert dialog or a snackbar
+                        if (mounted) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text(
+                                  'Unable to connect to the server. Please try again later.'),
+                            ),
+                          );
+                        }
+                      }
                     },
                     style: ElevatedButton.styleFrom(
                       foregroundColor: Colors.white,
-                      backgroundColor: Colors.blue, // Text color
+                      backgroundColor: Colors.blue,
                     ),
                     child: const Text('Login'),
                   )
@@ -93,16 +156,42 @@ class _LoginFormState extends State<LoginForm> {
   }
 }
 
-class SingUpForm extends StatefulWidget {
-  const SingUpForm({super.key});
+class SuccessScreen extends StatelessWidget {
+  const SuccessScreen({super.key});
 
-  @override
-  State<SingUpForm> createState() => _SingUpFormState();
-}
-
-class _SingUpFormState extends State<SingUpForm> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.lightGreen, // Bright green background
+      appBar: AppBar(
+        title: const Text("Login Successful"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => Navigator.pop(context),
+          child: const Text("Go Back"),
+        ),
+      ),
+    );
+  }
+}
+
+class FailScreen extends StatelessWidget {
+  const FailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.redAccent, // Red background for failure
+      appBar: AppBar(
+        title: const Text("Login Failed"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => Navigator.pop(context),
+          child: const Text("Go Back"),
+        ),
+      ),
+    );
   }
 }
